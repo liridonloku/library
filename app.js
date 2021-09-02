f8d9bd31c35f43db7d4f3b9fe6338f1dfddb8eaf
Liridon Loku <liridonloku@gmail.com>
2021-09-02 18:55:46 +0200

    Add functions to add,edit and remove books from library

--- app.js
+++ app.js
@@ -4,39 +4,161 @@ let myLibrary = [
         author: "George Orwell",
         pages: 360,
         read: "Yes"
-    },
-    {
-        title: "Animal Farm",
-        author: "George Orwell",
-        pages: 250,
-        read: "Yes"
     }
 ];
 
+//Book constructor
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(newBook){
+    myLibrary.push(newBook);
+}
+
+//create DOM card element for each book
 function createBookElement(object){
+    const card = document.createElement("div");
+    card.setAttribute('class', 'card');
+    card.setAttribute('data', myLibrary.indexOf(object).toString());
     const book = document.createElement("div");
     book.setAttribute('class', 'book');
     const title = document.createElement("p");
     title.textContent = object.title;
+    const titleDiv = document.createElement("div");
+    titleDiv.appendChild(title);
     const author = document.createElement("p");
     author.textContent = object.author;
+    const authorDiv = document.createElement("div");
+    authorDiv.appendChild(author);
     const pages = document.createElement("p");
-    pages.textContent = object.pages.toString();
+    pages.textContent = `Pages: ${object.pages.toString()}`;
+    const pagesDiv = document.createElement("div");
+    pagesDiv.appendChild(pages);
     const read = document.createElement("p");
-    read.textContent = object.read;
-    book.appendChild(title);
-    book.appendChild(author);
-    book.appendChild(pages);
-    book.appendChild(read);
-    return book;
+    read.textContent = `Read: ${object.read}`;
+    const readDiv = document.createElement("div");
+    readDiv.appendChild(read);
+    const readButton = document.createElement('button');
+    readButton.textContent = 'Read';
+    readButton.setAttribute('data', myLibrary.indexOf(object).toString());
+    readButton.addEventListener('click', toggleRead);
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.setAttribute('data', myLibrary.indexOf(object).toString());
+    deleteButton.addEventListener('click', deleteBook);
+    const buttonsDiv = document.createElement('div');
+    buttonsDiv.appendChild(readButton);
+    buttonsDiv.appendChild(deleteButton);
+    book.appendChild(titleDiv);
+    book.appendChild(authorDiv);
+    book.appendChild(pagesDiv);
+    book.appendChild(readDiv);
+    book.appendChild(buttonsDiv);
+    card.appendChild(book);
+    return card;
+}
+
+function toggleRead(){
+    if(myLibrary[this.attributes.data.value].read.toLowerCase() === "yes"){
+        myLibrary[this.attributes.data.value].read = "No";
+        updateLibrary();
+    }
+    else{
+        myLibrary[this.attributes.data.value].read = "Yes";
+        updateLibrary();
+    }
+}
+
+function deleteBook(){
+    myLibrary.splice(this.attributes.data.value,1);
+    updateLibrary();
+}
+
+//Create the '+' card
+function createAddNewElement(){
+    const addNew = document.createElement('div');
+    addNew.setAttribute('class', 'card');
+    addNew.setAttribute('id', 'add-new');
+    const newBook = document.createElement('div');
+    newBook.setAttribute('class', 'add-book');
+    newBook.setAttribute('id', 'new-book');
+    const text = document.createElement('h1');
+    text.textContent = '+';
+    newBook.appendChild(text);
+    addNew.appendChild(newBook);
+    return addNew;
 }
 
 function updateLibrary(){
     const library = document.querySelector('.library');
+    library.replaceChildren();
     myLibrary.forEach( book => {
         let bookElement = createBookElement(book);
         library.appendChild(bookElement);
     })
+    const addNewCard = createAddNewElement();
+    library.appendChild(addNewCard);
+    const plusButton = document.getElementById('new-book');
+    plusButton.addEventListener('click', toggleForm);
 }
+
+function toggleForm(){
+    const form = document.getElementById('form');
+    if(form.style.display === 'none' || form.style.display === ''){
+        form.style.display = 'flex';
+    }
+    else{
+        form.style.display = 'none';
+    }
+}
+
+function addBook(){
+    const title = document.getElementById('title');
+    const author = document.getElementById('author');
+    const pages = document.getElementById('pages');
+    const read = document.getElementById('read');
+    if(title.value === '' || author.value === '' || pages.value === '' || read.value === ''){
+        alert('Please fill all the fields.');
+        return;
+    }
+    if(isNaN(pages.value) || parseFloat(pages.value)%1 != 0){
+        alert('Pages must be a whole number.');
+        return;
+    }
+    if(read.value.toLowerCase() != 'yes' && read.value.toLowerCase() != 'no'){
+        alert("Read must be 'yes' or 'no'.");
+        return;
+    }
+    const newBook = new Book(title.value,author.value,pages.value,read.value);
+    addBookToLibrary(newBook);
+    updateLibrary();
+    toggleForm();
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    read.value = '';
+}
+
+function cancelAdd(){
+    const title = document.getElementById('title');
+    const author = document.getElementById('author');
+    const pages = document.getElementById('pages');
+    const read = document.getElementById('read');
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    read.value = '';
+    toggleForm();
+}
+
+const addButton = document.getElementById('add-button');
+addButton.addEventListener('click', addBook);
+
+const cancelButton = document.getElementById('cancel');
+cancelButton.addEventListener('click', cancelAdd)
 
 updateLibrary();
